64671a3a6945f8631b9de8c97fc6782513ff0c35
RITESH-DEL <riteshrishu2005@gmail.com>
2026-06-23 11:05:12 +0530

    minor updates

--- index.js
+++ index.js
@@ -1,44 +1,82 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-// import session from "express-session";
-// import passport from "./config/passport.js";
+import swaggerUI from 'swagger-ui-express'
+import swaggerJSdoc from 'swagger-jsdoc'
 import userRoutes from "./modules/users/user.routes.js";
 import authRoutes from "./modules/auth/auth.routes.js";
 import facultyRoutes from "./modules/faculty/faculty.routes.js";
 import config from "./config/app.config.js";
+import prisma from './db.js';
 
 const app = new express();
 
+const swaggerSpec = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'coding platform api',
+      description: 'RESTful API for coding platform',
+      version: '1.0.0'
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        }
+      }
+    },
+    server: [
+      {
+        url: `http://localhost:${config.port}`,
+        description: 'development server',
+      }
+    ],
+  },
+  apis: ['./modules/**/*.routes.js']
+}
+
+/* Global Middleware */
 app.use(cors({
     origin: [
         'http://localhost:5173',  // add frontend production url later
     ],
     credentials: true
 }));
-
-// Global Middleware
-// app.use(passport.initialize());
-// app.use(passport.session());
 app.use(cookieParser());
 app.use(express.json());
+app.use(
+    '/api-docs',
+    swaggerUI.serve,
+    swaggerUI.setup(swaggerJSdoc(swaggerSpec))
+)
 
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: false
-//   })
-// );
 
-// passport.serializeUser(
-//   (user, done) => done(null, user)
-// );
+app.use(
+    '/api-docs',
+    swaggerUI.serve,
+    swaggerUI.setup(swaggerJSdoc(swaggerSpec))
+)
 
-// passport.deserializeUser(
-//   (user, done) => done(null, user)
-// );
+const ui = `
+<style>
+html {
+  font-family: cursive;
+  text-align: center;
+  color: white;
+  background-color: black;
+  font-size: 200%;
+}
+</style>
+<h1 style="margin-top:5%;">Welcome, Sire!</h1>
+<p>Swagger docs at: <a href="/api-docs">/api-docs</a></p>
+`
 
+app.get('/', (req, res) => {
+  res.send(ui);
+})
 
 // application-level-routes
 app.use('/auth', authRoutes);
@@ -54,8 +92,18 @@ app.use((err, req, res, next) => {
   });
 });
 
+
 if (config.node_env !== 'test') {
-  app.listen(config.port, () => {
-    console.log(`Server listening at http://localhost:${config.port}`);
-  });
+  prisma.$connect()
+    .then(() => {
+      console.log("Database connection successful!");
+      
+      app.listen(config.port, () => {
+        console.log(`Server listening at http://localhost:${config.port}`);
+      });
+    })
+    .catch((error) => {
+      console.error("Database connection failed:", error);
+      process.exit(1); 
+    });
 }
