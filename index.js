7dd1617631dc1aa4dd57c8712feb262601a0e4ea
RITESH-del <riteshrishu2005@gmail.com>
2026-07-10 14:42:28 +0530

    Add production URL to CORS middleware

--- index.js
+++ index.js
@@ -49,7 +49,8 @@ const limiter = rateLimit({
 /* Global Middleware */
 app.use(cors({
     origin: [
-        'http://localhost:5173',  // add frontend production url later
+        'http://localhost:5173',  
+        'https://eval-platform-ten.vercel.app/'  // add frontend production url later
     ],
     credentials: true
 }));
